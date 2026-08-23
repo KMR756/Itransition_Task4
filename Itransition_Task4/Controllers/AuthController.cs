534b07830b48a7dd89f7d9254029be27701e7295
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2026-08-24 03:08:34 +0600

    Design login page

---
+++ Itransition_Task4/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Itransition_Task4.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
